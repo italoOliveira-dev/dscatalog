90efcbf54cec2379a1fc18735af97ac8c40e6eac
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-27 20:41:06 -0300

    initial commit

---
+++ src/test/java/br/com/projeto/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
