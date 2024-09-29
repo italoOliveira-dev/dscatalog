808cbaffbe37ba6eaf849282ebe219fb99b50d8d
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-29 00:19:37 -0300

    feat(exception): adiciona manipulador de exception global

---
+++ src/main/java/br/com/projeto/dscatalog/web/handlerException/HandlerApiExceptions.java
@@ -0,0 +1,41 @@
+package br.com.projeto.dscatalog.web.handlerException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.projeto.dscatalog.models.services.exceptions.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class HandlerApiExceptions {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<MessageError> methodArgumentNotValidException(MethodArgumentNotValidException ex,
+      HttpServletRequest request, BindingResult result) {
+    HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+    log.error("Error - ", ex);
+    return ResponseEntity
+        .status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(new MessageError(request, status, "Campo(s) inválido(s)", result));
+  }
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<MessageError> entityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    
+    log.error("Error - ", ex);
+    return ResponseEntity
+          .status(status)
+          .contentType(MediaType.APPLICATION_JSON)
+          .body(new MessageError(request, status, ex.getMessage()));
+  }
+}
