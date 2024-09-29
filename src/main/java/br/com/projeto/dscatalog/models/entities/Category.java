753d1d889eae5ca2235c97fd053758d18d8c85de
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-29 18:55:22 -0300

    feat(category): adiciona dados de auditoria

--- src/main/java/br/com/projeto/dscatalog/models/entities/Category.java
+++ src/main/java/br/com/projeto/dscatalog/models/entities/Category.java
@@ -1,9 +1,14 @@
 package br.com.projeto.dscatalog.models.entities;
 
+import java.time.Instant;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +19,10 @@ public class Category {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private Instant createdAt;
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private Instant updatedAt;
 
   public Category() {
 
@@ -26,6 +35,16 @@ public class Category {
 
   public Category(String name) {
     this.name = name;
+  }
+
+  @PrePersist
+  public void prePersist(){
+    this.createdAt = Instant.now();
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    this.updatedAt = Instant.now();
   }
 
   public Long getId() {
@@ -42,6 +61,14 @@ public class Category {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Instant getCreatedAt() {
+    return createdAt;
+  }
+
+  public Instant getUpdatedAt() {
+    return updatedAt;
   }
 
   @Override
