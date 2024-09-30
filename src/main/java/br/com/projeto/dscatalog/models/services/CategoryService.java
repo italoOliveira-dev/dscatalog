8f9ff9f0a49b38b607b0c3e8484bbff36e879e1f
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-09-29 22:56:51 -0300

    feat(category): adiciona paginação no findall

--- src/main/java/br/com/projeto/dscatalog/models/services/CategoryService.java
+++ src/main/java/br/com/projeto/dscatalog/models/services/CategoryService.java
@@ -1,8 +1,9 @@
 package br.com.projeto.dscatalog.models.services;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,8 +28,8 @@ public class CategoryService {
   }
 
   @Transactional(readOnly = true)
-  public List<CategoryResponseDTO> findAll() {
-    return categoryRepository.findAll().stream().map(CategoryResponseDTO::fromCategory).toList();
+  public Page<CategoryResponseDTO> findAll(Pageable page) {
+    return categoryRepository.findAll(page).map(CategoryResponseDTO::fromCategory);
   }
 
   @Transactional(readOnly = true)
--- src/main/java/br/com/projeto/dscatalog/web/controllers/CategoryController.java
+++ src/main/java/br/com/projeto/dscatalog/web/controllers/CategoryController.java
@@ -1,9 +1,11 @@
 package br.com.projeto.dscatalog.web.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -44,9 +46,9 @@ public class CategoryController {
   }
   
   @GetMapping
-  public ResponseEntity<List<CategoryResponseDTO>> findAll() {
+  public ResponseEntity<Page<CategoryResponseDTO>> findAll(@PageableDefault(size = 5, sort = "id") Pageable page) {
     
-    List<CategoryResponseDTO> list = categoryService.findAll();
+    Page<CategoryResponseDTO> list = categoryService.findAll(page);
 
     return ResponseEntity.ok(list);
   }
